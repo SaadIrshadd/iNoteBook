89e6e16703e3d8ad7210fcde564a0911b2dc4d7f
SaadIrshadd <saad2005irshadd@gmail.com>
2025-06-24 07:42:30 +0500

    routes

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,8 @@ const app = express()
 const port = 3000
 
 //Available Routes
-app.get('/', (req, res) => {
-    res.send('Hello Saad!')
-})
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
