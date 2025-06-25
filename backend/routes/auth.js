baae651ef733a5ed64553e8f54b22679c306b69d
SaadIrshadd <saad2005irshadd@gmail.com>
2025-06-25 07:13:21 +0500

    change

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,36 +1,47 @@
-const express=require('express');
-const User=require('../models/User');
-const router=express.Router();
-const {body , validationResult} = require('express-validator');
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-//Create a User using: POST "/api/auth". Doesnt require authentication
-router.post("/",
+//Create a User using: POST "/api/auth/createuser". Doesnt require authentication
+router.post(
+  "/createuser",
   [
     body("name", "Enter a valid Name").isLength({ min: 3 }),
     body("email", "Enter a valid Email").isEmail(),
-    body("password", "Password must be atleast 5 characters long").isLength({min: 5}),
+    body("password", "Password must be atleast 5 characters long").isLength({
+      min: 5,
+    }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //returing BAD REQUEST if there is an error
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a unique value for email",
-          message: err.message,
-        });
+    //Check whether user with the same email exist
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry user with the same email exist" });
+      }
+      //Create new User
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+      res.json(user);
+    } 
+    catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some error occured")
+    }
   }
 );
 
-module.exports=router
+module.exports = router;
